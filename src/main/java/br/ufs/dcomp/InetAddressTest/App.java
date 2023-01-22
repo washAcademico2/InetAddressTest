43404568058763f6749f746adc48e47615eea746
washAcademico2 <washington.souza@academico@ufs.br>
2023-01-22 15:30:21 -0300

    address list

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -12,10 +12,13 @@ public class App
     {
         try{
             
-            String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
+            String name = "www.google.com";
+            InetAddress[] address = InetAddress.getAllByName(name);
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address.getHostAddress());
+            for (InetAddress inetAddress : address) {
+                System.out.println("Address:   " + inetAddress.getHostAddress());
+
+            }
 
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
